2961f183257fa2c4b2067c9242729aa2974ecf50
Ruslan Kukharchuk <ruslan1b@users.noreply.github.com>
2020-05-24 21:08:24 +0300

    Update Homework1.cs

--- Task1/Homework1.cs
+++ Task1/Homework1.cs
@@ -15,26 +15,16 @@ namespace CSharpCore
                 Read the value of a from console and calculate area and perimetr of square with length a.
                 Output obtained results
                  */
-                Console.WriteLine("Enter value a");
+                Console.WriteLine("Enter a length the side of the square:");
                 int a = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Enter value b");
-                int b = Convert.ToInt32(Console.ReadLine());
-
-                int sum = a + b;
-                Console.WriteLine($"sum: {sum}");
-                int sub = a - b;
-                Console.WriteLine($"sub: {sub}");
-                int mul = a * b;
-                Console.WriteLine($"mul: {mul}");
-
-                if (b > 0)
-                {
-                    int div = a / b;
-                    Console.WriteLine($"div: {mul}");
-                }
-                else
-                    Console.WriteLine("Сannot be divided by 0!");
+                
+                int areaSquare = a * a;
+                Console.WriteLine($"The area a square is: {areaSquare}");
+               
+                int perimetrSquare = a *4;
+                Console.WriteLine($"The perimetr a square is: {perimetrSquare}");
+               
 
                 /*
                  Define string variable name and integer value age.
@@ -42,44 +32,30 @@ namespace CSharpCore
                 Read the value name and output next question: "How old are you,(name)?".
                 Read age and write whole information. 
                  */
-                Console.WriteLine("How are you?");
-                string answer = Console.ReadLine();
-                Console.WriteLine($"You are {answer}");
+                Console.WriteLine("What is your name?");
+                string answerName = Console.ReadLine();
+
+                Console.WriteLine($"How old are you, {answerName}?");
+                int answerAge = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"You name is {answerName}");
+                Console.WriteLine($"You age is {answerAge}");
 
                 /*
                  Read double number r and calculate the length (l=2*pi*r),
                 area (S=pi*r*r) and volume (4/3*pi*r*r*r) of a circle of given r 2. 
  
                  */
-                Console.WriteLine(" Enter three type char values");
-
-                Console.WriteLine("Enter first value:");
-                char numberChar1 = Convert.ToChar(Console.ReadLine());
-
-                Console.WriteLine("Enter second value:");
-                char numberChar2 = Convert.ToChar(Console.ReadLine());
-
-                Console.WriteLine("Enter third value:");
-                char numberChar3 = Convert.ToChar(Console.ReadLine());
-
-                Console.WriteLine($"You enter: {numberChar1}, {numberChar2}, {numberChar3}");
-
+                Console.WriteLine(" Enter a radius of the circle");
                 
-                Console.WriteLine(" Enter two type int values");
+                double radius = Convert.ToDouble(Console.ReadLine());
 
-                Console.WriteLine("Enter first value:");
-                int number1 = Convert.ToInt32(Console.ReadLine());
+                var lenghtCircle = 2 * (Math.PI) * radius;
+                var areaCircle = (Math.PI) * radius * radius;
+                var volumeCircle = 4 / 3 * (Math.PI) * radius * radius * radius;                
 
-                Console.WriteLine("Enter second value:");
-                int number2 = Convert.ToInt32(Console.ReadLine());
-                if (number1 > 0 && number2 > 0)
-                {
-                    Console.WriteLine("Two numbers are poitive");
-                }
-                else
-                {
-                    Console.WriteLine("One fron number or both are negativ");
-                }
+                Console.WriteLine($"A circle of radius {radius} has: lenght: {lenghtCircle}, area: {areaCircle}, volume: {volumeCircle}");
+
 
             }
             catch (Exception e)
