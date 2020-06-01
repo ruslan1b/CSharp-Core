36aecb8fc1d72b910a4583ca0c4a2c4f2c7c59c4
Ruslan Kukharchuk <ruslan1b@users.noreply.github.com>
2020-06-01 23:26:06 +0300

    Add Homework4

---
+++ CSharpCore/Homework4.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCore
+{
+    /// <summary>
+    /// Create class Person.
+    ///    Class Person should consists of
+    ///             a) two private fields: name and birthYear(the birthday year).As a type for this field you may use DataTime type.)
+    ///              b) two properties for access to these fields (only get)
+    ///              c) default constructor and constructor with 2 parameters
+    ///              d) methods: -Age() - to calculate the age of person
+    ///                        - Input() - to input information about person
+    ///                        -ChangeName() - to change the name of person
+    ///                           - ToString()
+    ///                         - Output() - to output information about person(call ToString())
+    ///                           - operator== (equal by name)
+    ///     In the method Main() create 6 objects of Person type and input information about them.Then calculate and write to console the name and Age of each person; Change the name of persons, which Age is less then 16, to "Very Young".
+    ///Output information about all persons.
+    ///Find and output information about Persons with the same names(use ==)
+    /// </summary>
+    class Homework4
+    {
+    }
+
+    class PersonHW4
+    {
+        private string name;
+        private DateTime birthYear;
+
+        public PersonHW4() {}
+
+        public PersonHW4 (string _name, DateTime _bithYear) { }
+
+        public void Age()
+        {
+
+        }
+
+        public void Input()
+        {
+
+        }
+
+        public void ChangeName() { }
+
+        //override ToString
+        public override string ToString()
+        {
+
+            return ($"");
+        }
+    }
+}
---
+++ CSharpCore/Task7_1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCore
+{
+    class Task7_1
+    {
+        public void AnnType()
+        {
+            var student = new
+            {
+                Id = 1,
+                FirstName = "Jonn",
+                LastName = "Lenon",
+                Lists = new List<string> {                    }
+
+            };
+        }
+    }
+}
