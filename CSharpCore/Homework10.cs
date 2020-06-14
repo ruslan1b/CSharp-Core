6c74c79d6430f1920913a7b601d35933226a77a1
Ruslan Kukharchuk <ruslan1b@users.noreply.github.com>
2020-06-14 17:48:12 +0300

    Create Homework10.cs

---
+++ CSharpCore/Homework10.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCore
+{
+    /// <summary>
+    /// A) Create Console Application project.
+    ///Use classes Shape, Circle, Square from your previous homework.
+    ///Use Linq and string functions to complete next tasks:
+    ///1) Create list of Shape and fill it with 6 different shapes(Circle and Square).
+    ///2) Find and write into the file shapes with area from range[10, 100]
+    ///3) Find and write into the file shapes which name contains letter 'a'
+    ///4) Find and remove from the list all shapes with perimeter less then 5. Write resulted list into Console
+
+    ///B) Create Console Application project.
+    ///Prepare txt file with a lot of text inside(for example take you.cs file from previos homework)
+    ///Read all lines of text from file into array of strings.
+    ///Each array item contains one line from file.
+    ///Complete next tasks:
+    ///1) Count and write the number of symbols in every line.
+    ///2) Find the longest and the shortest line.
+    ///3) Find and write only lines, which consist of word "var"/
+    /// </summary>
+    class Homework10
+    {
+        public void DemoHomework10()
+        {
+
+        }
+
+        
+
+    }
+
+    
+
+}
